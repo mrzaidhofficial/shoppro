87b4bd3b5a5033c256427ea37b9b818b9eea47cf
mrzaidhofficial <mrzaidhofficial@gmail.com>
2026-05-24 19:19:03 +0530

    Add SMTP timeout settings to fix hanging connections

--- services/emailService.js
+++ services/emailService.js
@@ -17,7 +17,10 @@ function getTransporter() {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-      }
+      },
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000
     });
     
     console.log('Email service: SMTP configured successfully');
