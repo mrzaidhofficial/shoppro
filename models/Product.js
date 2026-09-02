b097e2f8c23aaf25b8bca584928f8a1a5f2dc062
mrzaidhofficial <mrzaidhofficial@gmail.com>
2026-09-02 15:39:50 +0530

    Replace Books category with Beauty Care in about, edit-product, and products route

--- models/Product.js
+++ models/Product.js
@@ -11,7 +11,7 @@ var productSchema = new mongoose.Schema({
     category: {
         type: String,
         required: true,
-        enum: ['Electronics', 'Apparels', 'Home & Garden', 'Accessories', 'Sports', 'Books', 'Other']
+        enum: ['Electronics', 'Apparels', 'Home & Garden', 'Accessories', 'Sports', 'Beauty Care', 'Other']
     },
     stock: { type: Number, required: true, min: 0, default: 0 },
     images: [{ type: String }],
